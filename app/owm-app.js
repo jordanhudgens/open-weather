2619d5386373ca77f42642d115d54b96567a9299
Jordan Hudgens <jordan@wow.com>
2014-07-19 12:41:30 -0600

    refactored routing

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,42 +1,9 @@
 angular.module('OWMApp', ['ngRoute'])
-	.value('owmCities' ['New York', 'Dallas', 'Chicago'])
+    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
     .config(function ($routeProvider) {
         $routeProvider.when('/', {
             templateUrl : './home.html',
             controller : 'HomeCtrl'
-        }).when('/cities', {
-	            templateUrl : './city.html',
-	            controller : 'CityCtrl',
-	            resolve : {
-	            	city: function(owmCities, $route, $location) {
-	            		var city = $route.current.params.city;
-	            		if (owmCities.indexOf(city) == -1) {
-	            			$location.path('/error');
-	            			return;
-	            		}
-	            		return city;
-	            	}
-	            }
-        }).when('/error', {
-    		template : '<p>Error page not found</p>'
-    	}).when('/city', {
-	    	templateUrl : './city.html',
-	    	controller : 'CityCtrl',
-	    	resolve : {
-	    		city : function(owmFindCity, $route) {
-	    			var city = $route.current.params.city;
-	    			return owmFindCity(city);
-	    		}
-	    	}
-		}).otherwise({
-			redirectTo : '/error'
-		}).run(function($rootScope, $location) {
-			$rootScope.$on('$routeChangeError', function() {
-				$location.path('/error');
-			});
-		});
-    }).controller('HomeCtrl', function($scope) {
-=======
         // Note this a big in the learning docs... there are only two urls, well three
         //  with the error. Since resolve expects to operate on the :city param
          
@@ -62,15 +29,16 @@ angular.module('OWMApp', ['ngRoute'])
             redirectTo : '/error'
         })
     })
+    // .run is a method on the module, like config and controller
     .run(function($rootScope, $location) {
             $rootScope.$on('$routeChangeError', function() {
                 $location.path('/error');
             });
     })
     .controller('HomeCtrl', function($scope) {
->>>>>>> debugging_routing
         //empty for now
     })
-    .controller('CityCtrl', function($scope, $routeParams, city) {
-    	$scope.city = $routeParams.city;
+    .controller('CityCtrl', function($scope, city) {
+        
+        $scope.city = city;
     });
